93ac92e8ff5990098d9f9db790e5c09920b9456e
caprisun7 <ryanliu@live.com>
2024-07-01 20:34:29 +0800

    Setup for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: '/factor-construction-game/',
+    basePath: '/factor-construction-game',
+    // Preserve any existing configurations you might have:
+    // For example:
+    // experimental: {
+    //   appDir: true,
+    // },
+  }
+  
+  export default nextConfig;
